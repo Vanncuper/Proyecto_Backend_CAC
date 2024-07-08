44a1f8b88854b52e5b2b04a8b44d4bce1bb2e5b7
Gustavo <gustavoarluna@gmail.com>
2024-07-07 21:54:30 -0500

    carga datos

--- Crear BD y Tabla.sql
+++ Crear BD y Tabla.sql
@@ -9,7 +9,7 @@ use bd_hallcross;
 CREATE TABLE `evento` (
   `id_evento` INT NOT NULL AUTO_INCREMENT,
   `titulo` VARCHAR(100) NOT NULL,
-  `fechA` DATE NOT NULL,
+  `fecha` DATE NOT NULL,
   `descripcion` VARCHAR(800) NOT NULL,
   `imagen` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id_evento`));
